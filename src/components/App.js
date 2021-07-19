13e8e61eff0ada1367b6b6c055799f7b8f460b13
IsaacCodes2021 <84209008+IsaacCodes2021@users.noreply.github.com>
2021-07-19 09:31:27 -0700

    commit tree 135642f79f4873f86648dfe65716469873843c2d

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
 import React from "react";
+import Header from "./Header";
 
 function App() {
-  return <div className="App">learn react!</div>;
+  return <div className="App">learn react!
+  <Header />
+  </div>;
 }
 
 export default App;
